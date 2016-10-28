d6b5de570a78fe7bbb20273fa15715749530cc5d
katya20 <kpanchenko21@gmail.com>
2016-10-28 08:45:52 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("time to add some firebase code!")
 
 var key1 = {
-  nestedKey1: "hi!
-addListItem("key1", key1)
+  nestedKey1: "hi!",
+  nestedKey2: "bye!",
+}
+
+addListItem("key1", key1.nestedKey1)
