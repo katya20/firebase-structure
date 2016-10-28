efa8fb34746dd11c1dfefc3a5906123394a8d7d2
katya20 <kpanchenko21@gmail.com>
2016-10-28 09:01:19 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -11,5 +11,8 @@ var key2 = {
   nestedkey1: {nestedkey2: {nestedKey5: {notAsNestedKey: "I'm not as nested!"}}, nestedkey3: {nestedKey4: {reallyNestedKey: "I'm deeply nested!"}}}
 }
 
-storeValue("key2", key2);
+storeValue("key2", key2)
+
+var key3: "just me!"
+
     
