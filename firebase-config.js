13eb7c8ce917fd3acef05832377bbfead0c16279
katya20 <kpanchenko21@gmail.com>
2016-10-28 08:34:01 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 var firebaseConfig = {
-  apiKey: "NOT A REAL KEY",
-  authDomain: "not-a-real-db.firebaseapp.com",
-  databaseURL: "https://not-a-real-db.firebaseio.com"	
+  apiKey: "AIzaSyD-U1vfBg6sjZdxjgzgNzB97dfFvNrcX38",
+  authDomain: "project-1-48f93.firebaseapp.com",
+  databaseURL: "https://project-1-48f93.firebaseio.com"	
 };
 
 firebase.initializeApp(firebaseConfig);
